1ff855d3523edae9a09a452a9902e09dbc884b71
Galindo Leite <galindoleitept@gmail.com>
2024-05-25 19:12:32 +0100

    Solving generateStaticProps

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { GetStaticPropsContext } from "next";
 import { Product } from "@/types/types";
 import ProductPage from "./ProductPage";
 import { products } from "@/data/products";
@@ -7,11 +6,11 @@ const fetchProductById = async (id: string): Promise<Product | null> => {
   return products.find((product) => product.id === parseInt(id, 10)) || null;
 };
 
-interface ServerProductPageProps {
+interface ProductPageProps {
   params: { id: string };
 }
 
-const ServerProductPage: React.FC<ServerProductPageProps> = async ({ params }) => {
+const ServerProductPage = async ({ params }: ProductPageProps) => {
   const product = await fetchProductById(params.id);
 
   if (!product) {
@@ -23,27 +22,9 @@ const ServerProductPage: React.FC<ServerProductPageProps> = async ({ params }) =
 
 // Função para gerar caminhos estáticos
 export async function generateStaticParams() {
-  const paths = products.map((product) => ({
+  return products.map((product) => ({
     id: String(product.id),
   }));
-
-  return paths;
-}
-
-// Função para buscar dados estáticos
-export async function generateStaticProps({ params }: GetStaticPropsContext) {
-  const id = params?.id as string;
-  const product = await fetchProductById(id);
-
-  if (!product) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { params },
-  };
 }
 
 export default ServerProductPage;
