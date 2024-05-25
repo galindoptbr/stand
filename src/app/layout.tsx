4610411f2190dba5f5e94801304b94ff0e3c0c8f
Galindo Leite <galindoleitept@gmail.com>
2024-05-25 01:10:22 +0100

    Project day 7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,16 @@ import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 
-export const metadata: Metadata = {
+interface CustomMetadata extends Metadata {
+  image?: string;
+}
+
+export const metadata: CustomMetadata = {
   title: "Olavo Wheels",
   description: "Olavo Scale wheels",
+  keywords:
+    "hotwheels, wheels, scale, Rodas Hot Wheels 3D, Customização Hot Wheels, Rodas personalizadas Hot Wheels, Acessórios Hot Wheels, Rodas 3D carros miniatura, Coleção de carros Hot Wheels, Comunidade colecionadores Hot Wheels, Carros Hot Wheels vintage, Hot Wheels 3D wheels, Hot Wheels customization, Hot Wheels 3D accessories, wheels for miniature cars, Hot Wheels collectors, Hot Wheels car collection, Rare Hot Wheels models, Hot Wheels collectors community, Hot Wheels collectors guide, Hot Wheels collectors events",
+  image: "/assets/images/apple-touch-icon.png",
 };
 
 export default function RootLayout({
@@ -19,7 +26,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
   );
 }
